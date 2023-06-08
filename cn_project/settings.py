4a7d810d152dbc0c593195b2a62a7eb3f5bdf894
kavi-24 <--replace-all>
2023-06-08 22:41:14 +0530

    Second commit

--- cn_project/settings.py
+++ cn_project/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../mysite/static/'
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # Default primary key field type
